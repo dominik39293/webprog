fa7d8e42c2d82b288938fc4c285ce643b519dca3
None <66879102+dominik39293@users.noreply.github.com>
2025-09-22 13:27:54 +0200

    Github pages fix

--- 1_change_event/script.js
+++ 1_change_event/script.js
@@ -6,7 +6,7 @@ cities.addEventListener("change", () => {
   demo.textContent = selected;
   const image = document.querySelector("#image");
   const img = document.createElement("img");
-  img.src = `./img/${selected}.jpg`;
+  img.src = `https://raw.githubusercontent.com/dominik39293/webprog/refs/heads/main/1_change_event/img/${selected}.jpg`;
   img.title = selected;
   img.alt = selected;
   if(image.firstChild){
