72df2b5160f53915253abe0c02f9d9cb93a1bd30
dominik39293 <hokedomi@gmail.com>
2025-12-08 13:59:25 +0100

    asd

--- 8_topjoy_kepek_forgatasaval/script.js
+++ 8_topjoy_kepek_forgatasaval/script.js
@@ -1,14 +1,57 @@
-const cap = document.querySelector("#kupak");
-const text = document.querySelector("#szoveg");
-const container = document.querySelector(".tarolo");
+const kupak = document.querySelector('#kupak');
+const szoveg = document.querySelector('#szoveg');
+const tarolo = document.querySelector('.tarolo');
 
-let texts = [];
+//console.log(kupak, szoveg, tarolo);
+const uzik = [];
+let zarvaVanE = true;
 
-window.addEventListener("DOMContentLoaded", async () => {
-  const resp = await fetch('https://raw.githubusercontent.com/dominik39293/webprog/refs/heads/main/7_topjoy_fetch/uzik.csv');
-  await resp.text().split("\r\n").map(x => x.trim()).forEach(sor => {
-    texts.push(sor);
-  });
-})
+// uzik.csv fetch
+window.addEventListener('DOMContentLoaded', async() => {
+    try {
+        const response = await fetch('./uzik.csv');
+        //console.log(response);
+        const text = await response.text();
+        //console.log(text);
 
-let isCapClosed = true;
+        const sorok = text.split('\n').map(sor => sor.trim());
+        //console.log(sorok);
+        
+        sorok.forEach(sor => {
+            uzik.push(sor);
+        });
+        
+    } catch (error) {
+        console.log(error);
+    }  
+});
+
+//console.log(uzik);
+
+// Egy aszinkron, ami végiglépked a képeken.
+const animateCap = async (start, end) => {
+    const step = start < end ? 1 : -1; // Eldönti, hogy előre (kinyit) vagy visszafelé (bezár) kell lépkedni.
+
+    // Addig megy, amíg el nem éri a cél képek. Pl. 0->10 kinyitás, vagy 10->0 visszazárás
+    for (let index = start; index !== end + step; index+=step) {
+        console.log(index);
+        
+        // Vár 13ms-ot mielőtt kicserélné a képet, ez sima animációt eredményez.
+        await new Promise(resolve => setTimeout(resolve, 13));
+        kupak.src = `./images/bottlecap_${index}.png`;
+    }
+}
+
+tarolo.addEventListener('click', async () => {
+    if(zarvaVanE) {
+        //animálás kinyitáskor
+        await animateCap(0, 10);
+        szoveg.textContent = uzik[Math.floor(Math.random() * uzik.length)]
+        zarvaVanE = false;
+    } else {
+        //animálás záráskor
+        szoveg.textContent = '';
+        await animateCap(10, 0);
+        zarvaVanE = true;
+    }
+});
